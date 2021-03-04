0ce81e72fcde782097c4f55f6190b3029d3e40e0
Danijel Stanic <danijel.stanich@gmail.com>
2021-03-04 09:17:44 +0100

    Moved css files to styles folder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './reset.css';
-import './main.css';
+import './styles/reset.css';
+import './styles/main.css';
 import getWeather from './app/weather';
 import getLocations from './app/geocoding';
