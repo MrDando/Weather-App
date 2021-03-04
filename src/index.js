985827d5190e9f59561c9c4a49aaade141c84d16
Danijel Stanic <danijel.stanich@gmail.com>
2021-03-04 09:16:02 +0100

    Added reset.css to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
+import './reset.css';
 import './main.css';
 import getWeather from './app/weather';
 import getLocations from './app/geocoding';
-
-const locations = getLocations('Springfield');
-console.log(locations);
