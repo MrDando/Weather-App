f6887f62eab4e4ed21ef43de950e3d16b513cc04
Danijel Stanic <danijel.stanich@gmail.com>
2021-03-08 11:25:32 +0100

    Modified webpack loader to handle SVG files

--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,32 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    module: {
-      rules: [
-        {
-          test: /\.html$/i,
-          loader: 'html-loader',
-        },
-        {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[hash].[ext]',
-                outputPath: 'images/',
-                publicPath: 'images/'
-              }
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/',
             },
-          ],
-        },
-      ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: "./src/template.html"
-        }),
-      ],
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
 
-}
+};
