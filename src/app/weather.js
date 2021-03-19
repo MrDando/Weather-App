8e65bab8e4e44861956a927cd70abfdc2c8e234c
Danijel Stanic <danijel.stanich@gmail.com>
2021-03-19 14:06:34 +0100

    Fixed reported bugs

--- src/app/weather.js
+++
@@ -1,9 +0,0 @@
-async function getWeather(apiKey, lat, lon, units) {
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
-
-export { getWeather as default };
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './styles/reset.css';
-import './styles/main.css';
-
-import init from './app/app';
-
-init();
